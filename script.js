651f2f44ebc751fc0a04609197631e5f92f3cbc7
EmanuelyChaves <emanuely.chaves.santos@escola.pr.gov.br>
2025-08-25 08:52:33 -0300

    eshdjfgsghdfggf

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras= document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click",mostraPalavraChave);
+function mostraPalavraChave(){
+    const texto= document.querySelector("#entrada-de-texto").value;
+    const campoResultado= document.querySelector("#resultado-palavrachave");
+    const palavrachave= processaTexto(texto);
+    campoResultado.textContent= palavraChave.join(",");
+}
