a404ab22491b84f38c93dd7a1fa29bb5c37592d8
EmanuelyChaves <emanuely.chaves.santos@escola.pr.gov.br>
2025-09-15 08:01:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const botaoMostraPalavras= document.querySelector("#botao-palavrachave");
-botaoMostraPalavras.addEventListener("click",mostraPalavraChave);
-function mostraPalavraChave(){
-    const texto= document.querySelector("#entrada-de-texto").value;
-    const campoResultado= document.querySelector("#resultado-palavrachave");
-    const palavrachave= processaTexto(texto);
-    campoResultado.textContent= palavraChave.join(",");
-}
-function processaTexto(texto){
-    let palavras= texto.split(/\P(L)+/u);
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-    return palavras;
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u); 
+
+  return palavras;
 }
